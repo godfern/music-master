4e9a8eabfa9f7fe55e7eace5ffbc2aaa02741ff3
Godfrey F <godfern13@gmail.com>
2026-05-14 01:02:46 +0530

    Vercel fixes

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Avoid ENOENT on deps_temp_* when node_modules is churned or two dev servers run.
+  cacheDir: '.vite',
 })
